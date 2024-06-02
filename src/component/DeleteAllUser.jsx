b7425c2ffe17e52d4cbfb35967030cef0b12f5cd
bhagyashree-ghude <bhagyashreeghude@gmail.com>
2024-06-02 11:04:20 +0530

    delete all users

--- src/component/DeleteAllUser.jsx
+++ src/component/DeleteAllUser.jsx
@@ -1,14 +1,22 @@
 import { useDispatch } from "react-redux";
-import { deleteUser } from "./store/userSlice";
+import { deleteUsers } from "./store/userSlice";
 
 const DeleteAllUser = () => {
-  const dispatch = useDispatch()
-  const popAllUser =()=>{
-    dispatch(deleteUser)
-  }
+  const dispatch = useDispatch();
+  const delAllUser = () => {
+    console.log("hi");
+    dispatch(deleteUsers());
+  };
   return (
     <div>
-      <button className="bg-red-600 hover:bg-red-400 border-black p-2 m-2 rounded-lg" onClick={()=>{popAllUser()}}>DeleteAllUser</button>
+      <button
+        className="bg-red-600 hover:bg-red-400 border-black p-2 m-2 rounded-lg"
+        onClick={() => {
+          delAllUser();
+        }}
+      >
+        DeleteAllUser
+      </button>
     </div>
   );
 };
--- src/component/store/userSlice.jsx
+++ src/component/store/userSlice.jsx
@@ -18,8 +18,9 @@ const userSlice = createSlice({
             state.splice(action.payload,1)
 
         },
-        deleteUsers (state,action){
-            state.length=0(action.payload)
+        deleteUsers(state,action){
+            return  [];
+            // console.log("hi del")
             
         }
     }
@@ -27,6 +28,5 @@ const userSlice = createSlice({
 console.log(userSlice.actions)
 
 export default userSlice.reducer;
-export const {addUser} =userSlice.actions;
-export const {removeUser} =userSlice.actions;
-export const {deleteUser} =userSlice.actions;
+export const {addUser,removeUser,deleteUsers} =userSlice.actions;
+
